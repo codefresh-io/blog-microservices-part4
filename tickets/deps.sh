30f1d5ae6d21248bc899c1051dce06e21d62db3a
Oleg verhovsky <verchol@gmail.com>
2017-02-20 19:31:01 +0200

    fix

--- tickets/deps.sh
+++ tickets/deps.sh
@@ -5,4 +5,4 @@ git init;git remote add -f origin https://github.com/codefresh-io/blog-microserv
 git pull --depth=1 origin microservices_split;
 cd ..;
 npm i ./registry_micro/registry
-npm i ./registry_micro/logger.js
+npm i ./registry_micro/logger
